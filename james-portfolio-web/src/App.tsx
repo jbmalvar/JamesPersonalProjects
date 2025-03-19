9f693aeb9df42a4cb18540b2ad066d79b394b462
Yeli.James <jbmalvar.cs.washington.edu>
2025-03-18 23:05:19 -0700

    JamesPortfolioWebsite

---
+++ james-portfolio-web/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+        <div className = "header">
+            <img></img>
+            <h2>About</h2>
+            <h2>Projects</h2>
+            <h2>Skills</h2>
+        </div>
+    </div>
+  )
+}
+
+export default App
