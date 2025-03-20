df7673fce0f481e868d176115241ef2da585b844
Yeli.James <jbmalvar.cs.washington.edu>
2025-03-19 23:07:13 -0700

    Done for today feel unmotivated

--- james-portfolio-web/src/App.tsx
+++ james-portfolio-web/src/App.tsx
@@ -1,15 +1,23 @@
+import { useState } from 'react'
 import yellowGibus from './assets/yellowGibus.webp'
 import James from './assets/James.jpg'
 import './App.css'
 
 function App() {
+  const scrollTo = (sectionId: string) => {
+    const section = document.getElementById(sectionId)
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' })
+    }
+  }
+
   return (
     <div className="App">
         <nav className = "JamesNav">
           <img src = {yellowGibus} className = "jamesLogo"></img>
-          <h2 className = "barCat">About</h2>
+          <h2 className = "barCat" onClick={() => scrollTo("about-section")}>About</h2>
           <h2 className = "barCat">Projects</h2>
-          <h2 className = "barCat">Skills</h2>
+          <h2 className = "barCat" onClick={() => scrollTo("skills-section")}>Skills</h2>
           <h2 className = "right">Contact Me</h2>
           <a href="https://www.linkedin.com/in/james-joshua-malvar/" target="_blank" rel="noopener noreferrer">
             <img src="https://www.shareicon.net/data/2016/07/13/606885_linkedin_2048x2048.png" className="contacts" />
@@ -31,8 +39,16 @@ function App() {
             <p>Current Project: Personal Portfolio</p>
           </span>
         </div>
-        <div className = "SkillsContainer">
-            <h1>Skills</h1>
+        <div id = "about-section" className = "AboutContainer">
+            <h1>About Me</h1>
+            <p>I’m James as you can tell from above. I’m currently attending the University of Washington and I’m a Computer Science Major. I’m a third year there. I’m currently looking for an internship or some sort of experience.</p>
+            <p>I have a few hobbies. I watch anime and play a lot of video games. One of my favorite anime currently is Kusuriya No Hitorigoto. I love play video games as well and will play multiplayer games with friends regardless of the content. My favorite genre of single player games are turn based games. I’ve played the Persona series, Baldur’s Gate 3, and Fear and Hunger. Games I’ve enjoyed thoroughly.</p>
+            <p>
+            Enough about though, come learn about my skills and what I’ve worked on. If you’d like to contact me, check above.</p>
+        </div>
+        <div id = "skills-section" className = "SkillsContainer">
+          <h1>Skills</h1>
+          <p>Here are some of the skills I have</p>
         </div>
     </div>
   )
